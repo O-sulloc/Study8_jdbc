171ad76c86efbb1f38fd86123489bf3d75760d16
O-sulloc <bujjaf@gmail.com>
2022-01-19 12:36:32 +0900

    add_2_main

---
+++ src/com/jh/s1/start/StartMain.java
@@ -0,0 +1,11 @@
+package com.jh.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		System.out.println("DB 연동 테스틋 시작");
+
+		System.out.println("DB 연동 테스틋 끝");
+	}
+
+}
